699a456584f038419170198e3e2dca00ce0da0e3
JessAskeland <36720058+JessAskeland@users.noreply.github.com>
2018-04-06 15:47:42 -0500

    Renter

--- src/main/java/edu/dmacc/spring/vehicleaskelandgoehring/Renter.java
+++ src/main/java/edu/dmacc/spring/vehicleaskelandgoehring/Renter.java
@@ -1,12 +1,16 @@
 package edu.dmacc.spring.vehicleaskelandgoehring;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="renter")
-
 public class Renter {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int renterId;
 	private String firstName;
 	private String lastName;
